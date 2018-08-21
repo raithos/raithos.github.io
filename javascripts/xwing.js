2a08bd8e7c86b75daa785bf7ee37761e1c565dfd
raithos <raithos@users.noreply.github.com>
2018-08-21 13:17:39 -0400

    missed a text

--- javascripts/xwing.js
+++ javascripts/xwing.js
@@ -7626,7 +7626,7 @@ exportObj.cardLoaders.English = function() {
       text: "At the start of the Engagement Phase, if you are damaged, you may perform a red %REINFORCE% action."
     },
     "Mist Hunter": {
-      text: "Add %CANNON% slot."
+      text: "<i>Adds: %BARRELROLL% </i> %LINEBREAK% Add %CANNON% slot."
     },
     "Moff Jerjerrod": {
       text: "<i>Requires: %COORDINATE%</i> %LINEBREAK% During the System Phase, you may spend 2 %CHARGE%. If you do, choose the (1 %BANKLEFT%), (1 %STRAIGHT%), or (1 %BANKRIGHT%) template. Each friendly ship may perform a red %BOOST% action using that template."
